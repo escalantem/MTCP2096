3f6a4eba3533d6a9950af78c13d14a3b709ad07e
escalantem <escalantem@uninorte.edu.co>
2022-09-24 09:07:27 -0500

    Se agreg'o base html y wft.

--- CP10/app/app.py
+++ CP10/app/app.py
@@ -1,27 +1,55 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import utils, os
+from forms import FormInicio
+
+
 
 app = Flask(__name__)
 #app.config['DEBUG'] = True
 app.debug = True
+app.secret_key = 'c0v1-d1sp4p3l3s#2022' #os.urandom(24)  #'Hola mundo'
 
 if __name__ =='__main__':  
     app.run(debug = True)
 
-info= {'titulo': 'index',
-    'mensaje': 'Hola mundo jinja2'
-}
 
 @app.route('/')
 def index():
-    return render_template('index.html', datos=info)
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    formulario = FormInicio()
+    return render_template('iniciar_sesion.html', form=formulario)
+
 
 @app.route('/registro')
 def registro():
+    
+    if request.method == 'POST':
+        userName = request.form['user']
+        password = request.form['password']
+        email = request.form['userMail']
+        error =[]
+
+        if not utils.isUsernameValid(userName):
+            error.append('El usuario debe ser alfanumerico, o incluir . , - _')
+
+        if not utils.isPasswordValid(password):
+            error.append('La clave debe contener al menos una minuscula, una mayuscula y longitud de 8 caracteres')
+        
+        if not utils.isEmailValid(email):
+            error.append('Correo no valido.')
+        
+        
+        
+        return render_template('formulario.html', errorMessages=error)
+    
+
     return render_template('formulario.html')
 
-@app.route('/OtraPagina')
-def OtraPagina():
-    return 'OtraPagina'
+
+
     
 
     
